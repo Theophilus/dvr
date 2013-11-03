29140f0feaf966141d7cd7823d3b3e0611ad9898
Theophilus <aimthesun@yahoo.com>
2013-11-03 16:21:16 -0500

    Backend Done

--- cryptctl.c
+++ cryptctl.c
@@ -15,20 +15,37 @@ MODULE_LICENSE("GPL");
 
 int minorcount = 0; //count of minors created
 int i = 0;
+int current_minor = 0;
 //variables to create the main device
 static int times = 0;
 static dev_t first;
 static struct class *cl;
 static struct cdev c_dev;
 //variables to create a subdevice
-static dev_t sub;
+static dev_t ensub;
+static dev_t desub;
 static dev_t endev[16];
-static struct cdev sub_c;
-char subname[64];
+static dev_t dedev[16];
+static struct cdev ensub_c;
+static struct cdev desub_c;
+
+char enname[10];
+char dename[10];
 
 int dev_open(struct inode *, struct file *filep);
 int dev_rls(struct inode *, struct file *filep);
 static long crypt_ioctl(struct file *f, unsigned int cmd, unsigned long arg);
+
+static ssize_t en_write(struct file *filp, const char *buff, size_t len, loff_t *off);
+//static ssize_t en_read(struct file *filp, char *buff, size_t len, loff_t *off);
+//static ssize_t de_read(struct file *filp, char *buff, size_t len, loff_t *off);
+
+typedef struct dataset {
+  char *key;
+  char *words;
+}dataset;
+
+dataset data_table[16];
 
 //function operations for the main device
 static struct file_operations fops = {
@@ -36,6 +53,15 @@ static struct file_operations fops = {
   .release = dev_rls,
   .unlocked_ioctl = crypt_ioctl,
 };
+
+static struct file_operations en_ops = {
+  .write = en_write,
+  //.read = en_read,
+};
+
+/*static struct file_operations de_ops = {
+  .read = de_read,
+  };*/
 
 //These are the operations from ioctl
 //User program calls ioctl with a command which gets switch cased
@@ -48,28 +74,45 @@ static long crypt_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
       //limit of 16 minor devices
       if (minorcount < 16){
 	printk(KERN_INFO "called read\n");
-	sprintf(subname, "Encrypt%d",minorcount);
+	sprintf(enname, "Encrypt%d",minorcount);
+	sprintf(dename, "Decrypt%d",minorcount);
 
 	//These next two lines created a dev_t for the current minor
-	int major = MAJOR(sub);
-	endev[minorcount] = MKDEV(major, minorcount);
+	int enmajor = MAJOR(ensub);
+	int demajor = MAJOR(desub);
+	endev[minorcount] = MKDEV(enmajor, minorcount);
+	dedev[minorcount] = MKDEV(demajor, minorcount);
 
 	//These next lines run exactly like the main device creation
 	//except they're for a minor device
 	//They use the same module class but have a different dev_t
 	//They will also eventually have their own fops
-	if (device_create(cl, NULL, endev[minorcount], NULL, subname) == NULL)
+	if (device_create(cl, NULL, endev[minorcount], NULL, enname) == NULL)
 	  {
 	    class_destroy(cl);
 	    unregister_chrdev_region(endev[minorcount], 1);
 	    return -1;
 	  }
-	cdev_init(&sub_c, &fops);
-	if(cdev_add(&sub_c, endev[minorcount], 1) == -1)
+	if (device_create(cl, NULL, dedev[minorcount], NULL, dename) == NULL)
+	  {
+	    class_destroy(cl);
+	    unregister_chrdev_region(dedev[minorcount], 1);
+	    return -1;
+	  }
+	cdev_init(&ensub_c, &en_ops);
+	cdev_init(&desub_c, &fops);
+	if(cdev_add(&ensub_c, endev[minorcount], 1) == -1)
 	  {
 	    device_destroy(cl, endev[minorcount]);
 	    class_destroy(cl);
 	    unregister_chrdev_region(endev[minorcount], 1);
+	    return -1;
+	  }
+	if(cdev_add(&desub_c, dedev[minorcount], 1) == -1)
+	  {
+	    device_destroy(cl, dedev[minorcount]);
+	    class_destroy(cl);
+	    unregister_chrdev_region(dedev[minorcount], 1);
 	    return -1;
 	  }
 	minorcount++;  //minorcount incremented for next create
@@ -82,6 +125,7 @@ static long crypt_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
       
     case CRYPT_WRITE:
       printk(KERN_INFO "called write\n"); //this isn't implemented yet
+      current_minor = arg;
       break;
 
     default:
@@ -94,7 +138,11 @@ int init_module(void)
   printk(KERN_INFO "init_module() called\n");
   //next two if statements try to create the character device regions
   //for the main device and the sub devices
-  if (alloc_chrdev_region(&sub, 0, 1, "Encrypt") < 0)
+  if (alloc_chrdev_region(&ensub, 0, 1, "Encrypt") < 0)
+    {
+      return -1;
+    }
+  if (alloc_chrdev_region(&desub, 0, 1, "Decrypt") < 0)
     {
       return -1;
     }
@@ -108,8 +156,10 @@ int init_module(void)
   //from the last part
   if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL)
     {
-      unregister_chrdev_region(sub, 1);
+      unregister_chrdev_region(ensub, 1);
+      unregister_chrdev_region(desub, 1);
       unregister_chrdev_region(first, 1);
+
       return -1;
     }
   //This creates the actual device for the main device
@@ -117,7 +167,8 @@ int init_module(void)
   if (device_create(cl, NULL, first, NULL, "cryptctl") == NULL)
     {
       class_destroy(cl);
-      unregister_chrdev_region(sub, 1);
+      unregister_chrdev_region(ensub, 1);
+      unregister_chrdev_region(desub, 1);
       unregister_chrdev_region(first, 1);
       return -1;
     }
@@ -130,7 +181,8 @@ int init_module(void)
     {
       device_destroy(cl, first);
       class_destroy(cl);
-      unregister_chrdev_region(sub, 1);
+      unregister_chrdev_region(ensub, 1);
+      unregister_chrdev_region(desub, 1);
       unregister_chrdev_region(first, 1);
       return -1;
     }
@@ -142,18 +194,22 @@ void cleanup_module(void)
 {
   printk(KERN_INFO "cleanup_module() called\n");
   cdev_del(&c_dev);
-  cdev_del(&sub_c);
+  cdev_del(&ensub_c);
+  cdev_del(&desub_c);
 
   for (i = 0; i <= minorcount; i++){
     device_destroy(cl, endev[i]);
+    device_destroy(cl, dedev[i]);
   }
   device_destroy(cl, first);
 
   class_destroy(cl);
   for (i = 0; i <= minorcount; i++){
     unregister_chrdev_region(endev[i],1);
+    unregister_chrdev_region(dedev[i],1);
   }
-  unregister_chrdev_region(sub,1);
+  unregister_chrdev_region(ensub,1);
+  unregister_chrdev_region(desub,1);
   unregister_chrdev_region(first,1);
   
   printk(KERN_INFO "chr dev unregistered\n");
@@ -171,3 +227,31 @@ int dev_rls(struct inode *inode, struct file *filep)
   printk(KERN_INFO "Device closed\n");
   return 0;
 }
+
+static ssize_t en_write(struct file *filp, const char *buff, size_t len, loff_t *off)
+{
+  //This converts the first char in the buffer to int.  This will be used to
+  //determine which minor pair to use.
+  current_minor = buff[0] - '0';
+  sprintf(data_table[current_minor].key, "DEFAULT");
+  sprintf(data_table[current_minor].words, "%s", buff);
+  //between 32 and 126
+  int i = 1;
+  int con = 0;
+  int hold = 0;
+  /*while (i < len) {
+    int b = (int)buff[i];
+    if( NULL)
+      {
+	con = 0;
+      }
+    int ke = (int)data_table[current_minor].key[con++];
+    hold = ((b + ke - 64) % 95) + 32;
+    data_table[current_minor].words[i] = (char)hold;
+    i++;
+    }*/
+  printk(KERN_INFO "%s", data_table[current_minor].words);
+  return len;
+}
+
+
--- test.c
+++ test.c
@@ -10,7 +10,7 @@
 
 #include "crypt_ioctl.h"
 
-int main()
+int main(int argc, char *argv[])
 {
   int fd = 0;
   int ret = 0;
@@ -21,7 +21,11 @@ int main()
   ioctl(fd, CRYPT_CREATE);
   ret = open("/dev/Encrypt0",O_RDWR);
   ioctl(ret, CRYPT_WRITE);
-  
+  //sprintf(buff, "%s%s", argv[1],argv[2]);
+  //printf("%s\n", buff);
+  //printf("%d\n", (int)strlen(buff));
+  //write(ret, buff, strlen(buff));
+
   close(fd);
   close(ret);
 }
